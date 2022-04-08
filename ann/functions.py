Finally fixed backpropagation

ann/functions.py
@@ -11,28 +11,46 @@ class ActivationFunction:
         self.derivative = derivative
         self.name = name
 
-    def apply_on_tensor(self, tensor):
+    def apply_on_tensor(self, tensor: np.ndarray) -> np.ndarray:
         return self.function(tensor)
 
-    def get_gradient_from_tensor(self, tensor):
+    def get_gradient_from_tensor(self, tensor: np.ndarray) -> np.ndarray:
         return self.derivative(tensor)
 
-    def __call__(self, tensor, gradient=False):
+    def __call__(self, tensor: np.ndarray, gradient=False) -> np.ndarray:
         return self.apply_on_tensor(tensor) if not gradient else self.derivative(tensor)
 
 
-LINEAR = ActivationFunction(np.vectorize(lambda x: x),
-                            np.vectorize(lambda x: 1),
-                            "LINEAR")
-RELU = ActivationFunction(lambda x: np.max(x, 0),
-                          np.vectorize(lambda x: int(x > 0)),
-                          "RELU")
-TANH = ActivationFunction(lambda x: np.tanh(x),
-                          lambda x: 1 - np.tanh(x) ** 2,
-                          "TANH")
-SIGMOID = ActivationFunction(lambda x: 1 / (1 + np.exp(-x)),
-                             lambda x: np.exp(-x) / (1 + np.exp(-x)) ** 2,
-                             "SIGMOID")
+LINEAR = ActivationFunction(
+    np.vectorize(lambda x: x), 
+    np.vectorize(lambda x: 1), 
+    "LINEAR"
+)
+RELU = ActivationFunction(
+    np.vectorize(lambda x: max(x, 0.0)),
+    np.vectorize(lambda x: float(x > 0)),
+    "RELU"
+)
+LEAKY_RELU = ActivationFunction(
+    np.vectorize(lambda x: max(x, 0.1*x)),
+    np.vectorize(lambda x: 1.0 if x >= 0 else 0.1),
+    "LeakyRELU"    
+)
+SOFTPLUS = ActivationFunction(
+    lambda x: np.log(1 + np.exp(x)),
+    lambda x: 1 / (1 + np.exp(-x)),
+    "SOFTPLUS"
+)
+TANH = ActivationFunction(
+    lambda x: np.tanh(x),
+    lambda x: 1 - np.tanh(x) ** 2,
+    "TANH"
+)
+SIGMOID = ActivationFunction(
+    lambda x: 1 / (1 + np.exp(-x)),
+    lambda x: np.exp(-x) / (1 + np.exp(-x)) ** 2,
+    "SIGMOID"
+)
 
 
 def softmax_processing_function(tensor: np.ndarray) -> np.ndarray:
@@ -44,26 +62,42 @@ SOFTMAX = ActivationFunction(softmax_processing_function, None, "SOFTMAX")
 
 
 class LossFunction:
-    def __init__(self, function: Callable[[np.ndarray, np.ndarray], np.ndarray], name: str):
+    def __init__(self, function: Callable[[np.ndarray, np.ndarray], np.ndarray], derivative: Callable[[np.ndarray, np.ndarray], np.ndarray], name: str):
         self.function = function
+        self.derivative = derivative
         self.name = name
 
-    def calculate_loss(self, obtained: np.ndarray, expected: np.ndarray):
+    def calculate_loss(self, obtained: np.ndarray, expected: np.ndarray) -> np.ndarray:
         return self.function(obtained, expected)
 
-    def __call__(self, obtained: np.ndarray, expected: np.ndarray):
-        return self.calculate_loss(obtained, expected)
+    def calculate_gradient(self, obtained: np.ndarray, expected: np.ndarray) -> np.ndarray:
+        return self.derivative(obtained, expected)
+
+    def __call__(self, obtained: np.ndarray, expected: np.ndarray, gradient = False) -> np.ndarray:
+        return self.calculate_loss(obtained, expected) if not gradient else self.calculate_gradient(obtained, expected)
 
 
 def multi_class_log_loss(obtained_val: float, expected_val: float) -> float:
     return -expected_val * np.log(obtained_val) if expected_val != 0 else 0
 
 
-MSE = LossFunction(lambda t1, t2: (t1 - t2) ** 2, "MEAN SQUARED ERROR")
-MAE = LossFunction(lambda t1, t2: np.abs(t1 - t2), "MEAN ABSOLUTE ERROR")
-MCCEL = LossFunction(np.vectorize(multi_class_log_loss), " MULTI CLASS CROSS ENTROPY LOSS")
+MSE = LossFunction(
+    lambda obtained, expected: ((obtained - expected) ** 2) / 2,
+    lambda obtained, expected: obtained - expected, 
+    "MEAN SQUARED ERROR"
+)
+MAE = LossFunction(
+    lambda obtained, expected: np.abs(obtained - expected),
+    lambda obtained, expected: (obtained > expected) * 2 - 1,
+    "MEAN ABSOLUTE ERROR"
+)
+MCCEL = LossFunction(
+    np.vectorize(multi_class_log_loss),
+    lambda obtained, expected: np.vectorize(multi_class_log_loss)(obtained, expected) - expected, 
+    "MULTI CLASS CROSS ENTROPY LOSS"
+)
 
 if __name__ == "__main__":
     expected = np.array(((1, 0, 0),)).T
     obtained = np.array(((0.5, 0.3, 0.2),)).T
-    print(MCCEL(obtained, expected))
+    print(MSE(obtained, expected))

ann/neural.py
@@ -12,6 +12,9 @@ def to_batches(data, batch_size):
     return batches
 
 
+labeled_entry = tuple[np.ndarray, np.ndarray]
+
+
 class Network:
     def __init__(self,
                  layers: tuple[int, ...],
@@ -19,98 +22,113 @@ class Network:
                  functions: tuple[f.ActivationFunction, ...],
                  loss_function: f.LossFunction):
         self.weight_sizes = [(layers[i + 1], layers[i]) for i in range(len(layers) - 1)]
-        self.bias_sizes = [(1, size) for size in layers[1:]]
+        self.bias_sizes = [size for size in layers[1:]]
         self.weights = [np.random.standard_normal(size) for size in self.weight_sizes]
         self.biases = [np.random.standard_normal(size) for size in self.bias_sizes]
         self.functions = functions
         self.learning_rate = learning_rate
         self.loss_function = loss_function
 
-    def calculate_all(self, provided_input):
-        current_output = np.array([provided_input]).T
-        outputs = []
+    def calculate_all(self, provided_input: np.ndarray) -> tuple[list[np.ndarray], list[np.ndarray]]:
+        current_output = provided_input
+        raw_outputs = []
+        func_outputs = []
         for weight, bias, function in zip(self.weights, self.biases, self.functions):
-            current_output = function(weight @ current_output + bias.T)
-            outputs.append(current_output)
-        return outputs
+            raw = weight @ current_output + bias.T
+            raw_outputs.append(raw)
+            current_output = function(raw)
+            func_outputs.append(current_output)
+        return raw_outputs, func_outputs
 
-    def calculate(self, a):
-        return self.calculate_all(a)[-1]
+    def calculate(self, input_vector: np.ndarray):
+        return self.calculate_all(input_vector)[1][-1]
 
-    def get_error(self, output, expected):
-        return self.loss_function(output, np.array((expected,)).T)
+    def get_error(self, output: np.array, expected: np.array) -> np.array:
+        return np.sum(self.loss_function(output, expected))
 
-    def calculate_errors(self, outputs, cls):
-        errors = [self.get_error(outputs[-1], cls)]
-        for weight in self.weights[:0:-1]:
-            errors.append(weight.T @ errors[-1])
-        return errors[::-1]
+    def calculate_gradients(self, raw_outputs, func_outputs, expected):
+        loss_gradient = self.loss_function(func_outputs[-1], expected, gradient=True)
+        gradients = [loss_gradient * self.get_derivative(raw_outputs, func_outputs, len(self.weights) - 1, expected)]
+        for idx, weight in enumerate(self.weights[:0:-1]):
+            gradients.append(
+                (weight.T @ gradients[-1]) * self.get_derivative(raw_outputs, func_outputs, len(self.weights) - idx - 2, expected)
+            )
+        return gradients[::-1]
 
-    def get_derivative(self, inputs, outputs, i, expected):
-        if self.functions[i] is f.SIGMOID:
-            return outputs[i] * (1 - outputs[i])
-        if self.functions[i] is f.SOFTMAX:
-            return outputs[i] - np.array((expected,)).T
-        outputs_vector = outputs[i - 1] if i > 0 else np.array([inputs]).T
-        return self.functions[i](np.matmul(self.weights[i], outputs_vector), gradient=True)
+    def get_derivative(self, raw_outputs, func_outputs, layer_index, expected):
+        if self.functions[layer_index] is f.SIGMOID:
+            return func_outputs[layer_index] * (1 - func_outputs[layer_index])
+        if self.functions[layer_index] is f.SOFTMAX:
+            return func_outputs[layer_index] - np.array((expected,)).T
+        return self.functions[layer_index](raw_outputs[layer_index], gradient=True)
 
-    def update_gradients_and_deltas(self, input_vector, expected, gradients, deltas):
-        outputs = self.calculate_all(input_vector)
-        errors = self.calculate_errors(outputs, expected)
-        for i in range(len(errors) - 1, -1, -1):
-            derivative = self.get_derivative(input_vector, outputs, i, expected)
-            temp = self.learning_rate * errors[i] * derivative
-            gradients[i] += temp
-            factor = outputs[i - 1].T if i > 0 else np.array(input_vector).T
-            deltas[i] += temp * factor
+    def update_gradients_and_deltas(self,
+                                    input_vector: np.ndarray,
+                                    expected: np.ndarray,
+                                    delta_weights: list,
+                                    delta_biases: list) -> None:
+        raw_outputs, func_outputs = self.calculate_all(input_vector)
+        gradients = self.calculate_gradients(raw_outputs, func_outputs, expected)
+        for i in reversed(range(1, len(func_outputs))):
+            delta_biases[i] += gradients[i]
+            delta_weights[i] += np.outer(gradients[i], func_outputs[i - 1].T)
+        delta_biases[0] += gradients[0]
+        delta_weights[0] += np.outer(gradients[0], input_vector)
 
-    def backpropagation(self, data):
-        if len(data) == 0:
-            return
-        gradients = [0] * len(self.weights)
-        deltas = [0] * len(self.weights)
-        for pair, cls in data:
-            self.update_gradients_and_deltas(pair, cls, gradients, deltas)
+    def backpropagation(self, training_data: list[labeled_entry], validation_data: list[labeled_entry]) -> float:
+        if len(training_data) == 0:
+            return 0.0
+        delta_weights = [0] * len(self.weights)
+        delta_biases = [0] * len(self.weights)
+        for entry, label in training_data:
+            self.update_gradients_and_deltas(entry, label, delta_weights, delta_biases)
 
-        for delta in deltas:
-            delta /= len(data)
-        for gradient in gradients:
-            gradient /= len(data)
-
-        for i in range(len(self.weights)):
-            self.weights[i] += deltas[i]
-            self.biases[i] += gradients[i].T
+        for i, _ in enumerate(delta_weights):
+            delta_weights[i] /= len(training_data)
+            delta_biases[i] /= len(training_data)
+            self.weights[i] -= self.learning_rate * delta_weights[i]
+            self.biases[i] -= self.learning_rate * delta_biases[i].T
             self.weights[i] = np.clip(self.weights[i], -10, 10)
             self.biases[i] = np.clip(self.biases[i], -10, 10)
+
+        return sum([self.get_error(self.calculate(entry), label) for entry, label in validation_data])
 
     def print_net(self):
         print("--------------------------------------------------", end='')
         for i, (weight_layer, bias_layer, func) in enumerate(zip(self.weights, self.biases, self.functions)):
-            print(f"\nLayer {i + 1} ({func.name})")
+            print(f"\nLayer {i + 1}")
             print("\tWeights:")
             for j, neuron_weights in enumerate(weight_layer):
                 print(f"\t\tNeuron {j}:\t{[round(w, 2) for w in neuron_weights]}")
-            print(f"\n\tBiases:\t{[round(b, 2) for b in bias_layer[0]]}")
+            print(f"\n\tBiases:\t{[round(b, 2) for b in bias_layer]}")
+            print(f"\n\tApplied Function: {func.name}")
         print("--------------------------------------------------")
 
 
 def main():
-    net = Network((2, 4, 1), 1, (f.SIGMOID, f.SIGMOID), loss_function=f.MAE)
-    initial_eval = net.calculate((0, 0))
-    print(initial_eval, net.get_error(initial_eval, 0))
-    initial_eval = net.calculate((0, 1))
-    print(initial_eval, net.get_error(initial_eval, 1))
-    data = [((1, 1), 0), ((0, 1), 1), ((0, 0), 0), ((1, 0), 1)]
-    for i in range(30):
+    data = [
+        (np.array([1, 1]), np.array(0)),
+        (np.array([0, 0]), np.array(0)),
+        (np.array([1, 0]), np.array(1)),
+        (np.array([0, 1]), np.array(1))
+    ]
+    np.random.seed(0)
+    net = Network((2, 2, 1), 1, (f.RELU, f.RELU), loss_function=f.MSE)
+    net.print_net()
+    val = net.calculate(data[-1][0])
+    net.backpropagation([data[-1]])
+    net.print_net()
+    net.calculate(data[-1][0])
+    print(val)
+    return
+    for pair, label in data:
+        print(pair, net.calculate(pair), label)
+
+    for i in range(300):
         net.backpropagation(data)
-    initial_eval = net.calculate((0, 0))
-    print(initial_eval, net.get_error(initial_eval, 0))
-    initial_eval = net.calculate((0, 1))
-    print(initial_eval, net.get_error(initial_eval, 1))
-    # print(net.calculate((1, 1)))
-    # print(net.calculate((1, 0)))
-    # print(net.calculate((0, 1)))
-    # print(net.calculate((0, 0)))
+    print('===========================')
+    for pair, label in data:
+        print(pair, net.calculate(pair), label)
 
 
 if __name__ == "__main__":